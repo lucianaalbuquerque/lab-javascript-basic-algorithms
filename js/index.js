8ba54bda2b4d4ffde9e6412cbe86715988bc0d5e
Luciana A <lucianaalbuquerque12@gmail.com>
2022-01-17 19:31:31 +0000

    iteration 3

--- js/index.js
+++ js/index.js
@@ -14,3 +14,23 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
+//3.1 
+let driverName = ''
+for (letter of hacker1) {
+  driverName += letter.toUpperCase() + " "
+}
+
+//3.2
+let a = hacker1.localeCompare(hacker2);
+console.log(a)
+
+let b = hacker2.localeCompare(hacker1);
+console.log(b)
+
+if (a > b) {
+  console.log("The driver's name goes first.")
+} else if (a < b) {
+  console.log("Yo, the navigator goes first definitely.")
+} else {
+    console.log("What?! You both have the same name?")
+}
